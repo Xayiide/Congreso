92926a1672e6eedfb82d125fc807274e938bc4cf
Xayiide <wickedcheerio@protonmail.com>
2021-11-04 00:42:50 +0100

    :peach:

--- Utils/Utils.py
+++ Utils/Utils.py
@@ -6,21 +6,32 @@ resColors = {'Sí'        : '#6EEB83',
              'Abstención': '#767676',
              'No vota'   : '#DCDCDC'}
 
+# Provisional
+gpColors = {'GP'   : '#1D84CE',
+            'GS'   : '#DC0000',
+            'GVOX' : '#63BE21',
+            'GPlu' : '#DCDCDC',
+            'GCs'  : '#EB6109'}
 
-def lbcFilter(agregados):
-      labels, sizes, colors = [], [], []
-      auxlabels = []
-      for voto in agregados:
-            if agregados[voto] != 0:
-                  auxlabels.append(str(voto))
-                  sizes.append(agregados[voto])
 
-      for i, auxlabel in enumerate(auxlabels):
-            colors.append(resColors[auxlabel])
-            num = agregados[auxlabel]
-            labels.append(str(auxlabel + " [" + str(num) + "]"))
+def lbcFilter(agregados, color=None):
+    palette = resColors
+    if color == 'partido':
+        palette = gpColors
 
-      return labels, sizes, colors
+    labels, sizes, colors = [], [], []
+    auxlabels = []
+    for voto in agregados:
+        if agregados[voto] != 0:
+            auxlabels.append(str(voto))
+            sizes.append(agregados[voto])
+
+    for i, auxlabel in enumerate(auxlabels):
+        colors.append(palette[auxlabel])
+        num = agregados[auxlabel]
+        labels.append(str(auxlabel + " [" + str(num) + "]"))
+
+    return labels, sizes, colors
 
 
 def createPieChart(labels, sizes, colors, titulo):
--- VotStats/VotStats.py
+++ VotStats/VotStats.py
@@ -19,6 +19,11 @@ class VotStats:
         self.votacion  = Votacion(self.data)
         self.votResultados()
         self.votosPorGrupo()
+        self.votSiPorGrupo()
+        self.votNoPorGrupo()
+        self.votAbsPorGrupo()
+        self.votNovPorGrupo()
+
 
         self.showCharts()
 
@@ -26,6 +31,7 @@ class VotStats:
     def votResultados(self):
         titulo    = "Resultados de la votación"
         agregados = self.votacion.votosAgregados
+        print(agregados)
         labels, sizes, colors = Utils.lbcFilter(agregados)
         return Utils.createPieChart(labels, sizes, colors, titulo)
 
@@ -37,6 +43,23 @@ class VotStats:
             agregados = grupos[grupo].votosAgregados
             labels, sizes, colors = Utils.lbcFilter(agregados)
             Utils.createPieChart(labels, sizes, colors, titulo)
+
+
+    def votSiPorGrupo(self):
+        gps = ['GP', 'GS', 'GVOX', 'GPlu', 'GCs']
+        for p in gps:
+            grupo = self.votacion.grupos[p]
+
+
+    
+    def votNoPorGrupo(self):
+        pass
+
+    def votAbsPorGrupo(self):
+        pass
+    
+    def votNovPorGrupo(self):
+        pass
 
     def showCharts(self):
         plt.tight_layout()
